67521ea1cc9b43145bfadccd864fdc313932b73b
Roberto Asiel Guevara Castañeda <robert04siel@gmail.com>
2018-06-21 22:54:40 -0400

    Fixed conditions with only an if block

--- src/generators/conditions.ts
+++ src/generators/conditions.ts
@@ -4,7 +4,7 @@ import { NodeElement, BlockAreas, Condition } from '../utilities/classes';
 import { getVarName, getParent, createElement, capitalize } from '../utilities/tools';
 
 export function genIf(node: NodeElement, areas: BlockAreas, scope: string) {
-	const ifCond = ctx(node.getAttribute('$if'), scope.split(',')[0], areas.globals || []);
+  const ifCond = ctx(node.getAttribute('$if'), scope.split(',')[0], areas.globals || []);
   const condition: Condition = { ifCond };
   const anchor = getVarName(areas.variables, 'conditionAnchor');
   const block = getVarName(areas.variables, 'conditionBlock');
@@ -15,22 +15,22 @@ export function genIf(node: NodeElement, areas: BlockAreas, scope: string) {
   } else {
     areas.create.push(`_$a(${root}, ${anchor});`);
   }
-	node.removeAttribute('$if');
+  node.removeAttribute('$if');
   areas.conditions.push(condition);
   areas.outer.push(genItemCondition(scope, node, areas, 'if'));
   areas.extras.push(`${anchor} = _$ct();`);
-	areas.extras.push(`${block} = condition_${areas.conditions.length}(${scope});`);
+  areas.create.push(`${block} = condition_${areas.conditions.length}(${scope});`);
   if (node.nextElementSibling.hasAttribute('$else-if')) {
     genElseIf(scope, node.nextElementSibling, condition, areas);
   }
   if (node.nextElementSibling.hasAttribute('$else')) {
     const sibling = node.nextElementSibling;
     condition.elseCond = condition.elseCond || true;
-		node.removeAttribute('$else');
+    sibling.removeAttribute('$else');
     areas.outer.push(genItemCondition(scope, sibling, areas, 'else'));
-		sibling.parentElement.removeChild(sibling);
+    parent.removeChild(sibling);
   }
-	node.parentElement.removeChild(node);
+  parent.removeChild(node);
   areas.outer.push(genCondition(scope, condition, areas.conditions.length));
   areas.create.push(`${block}.$create();`);
   areas.unmount.push(`${block}.$mount(${root || '_$frag'}, ${anchor});`);
@@ -39,15 +39,15 @@ export function genIf(node: NodeElement, areas: BlockAreas, scope: string) {
 }
 
 function genElseIf(scope: string, node: NodeElement, condition: Condition, areas: BlockAreas) {
-	const elseifCond = ctx(node.getAttribute('$else-if'), scope, []);
-	condition.elseIfConds = condition.elseIfConds || [];
-	condition.elseIfConds.push(elseifCond);
-	node.removeAttribute('$else-if');
-	areas.outer.push(genItemCondition(scope, node, areas, `elseIf_${condition.elseIfConds.length}`));
-	if (node.nextElementSibling.hasAttribute('$else-if')) {
-		genElseIf(scope, node.nextElementSibling, condition, areas);
-	}
-	node.parentElement.removeChild(node);
+  const elseifCond = ctx(node.getAttribute('$else-if'), scope, []);
+  condition.elseIfConds = condition.elseIfConds || [];
+  condition.elseIfConds.push(elseifCond);
+  node.removeAttribute('$else-if');
+  areas.outer.push(genItemCondition(scope, node, areas, `elseIf_${condition.elseIfConds.length}`));
+  if (node.nextElementSibling.hasAttribute('$else-if')) {
+    genElseIf(scope, node.nextElementSibling, condition, areas);
+  }
+  node.parentElement.removeChild(node);
 }
 
 function genItemCondition(scope: string, node: NodeElement, areas: BlockAreas, type?: string) {
@@ -75,33 +75,33 @@ function genItemCondition(scope: string, node: NodeElement, areas: BlockAreas, t
 }
 
 function genCondition(scope: string, { ifCond, elseIfConds, elseCond }: Condition, count: number) {
-	const funcName = `condition_${count}`;
-	let condition = `function ${funcName}(${scope}) {`;
-	if (ifCond) {
-		condition += `if (${ifCond}) return if${capitalize(funcName)}(${scope});`;
-	}
-	if (elseIfConds) {
-		for (let i = 0; i < elseIfConds.length; i++) {
-			const elseIfCond = elseIfConds[i];
-			condition += `else if (${elseIfCond}) return elseIf_${i + 1}_${funcName}(${scope});`;
-		}
-	}
-	if (elseCond) {
-		condition += `else return else${capitalize(funcName)}(${scope});`;
-	} else {
-		condition += `else return {
+  const funcName = `condition_${count}`;
+  let condition = `function ${funcName}(${scope}) {`;
+  if (ifCond) {
+    condition += `if (${ifCond}) return if${capitalize(funcName)}(${scope});`;
+  }
+  if (elseIfConds) {
+    for (let i = 0; i < elseIfConds.length; i++) {
+      const elseIfCond = elseIfConds[i];
+      condition += `else if (${elseIfCond}) return elseIf_${i + 1}_${funcName}(${scope});`;
+    }
+  }
+  if (elseCond) {
+    condition += `else return else${capitalize(funcName)}(${scope});`;
+  } else {
+    condition += `else return {
 			type: 'empty-else', $create: _$noop, $mount: _$noop, $update: _$noop, $destroy: _$noop
 		};`;
-	}
-	condition += '}';
-	return condition;
+  }
+  condition += '}';
+  return condition;
 }
 
 function genConditionUpdate(scope: string, root: string, block: string, anchor: string, cond: number) {
-	return `if (${block}.type === condition_${cond}(${scope}).type) {
+  return `if (${block} && ${block}.type === condition_${cond}(${scope}).type) {
 		${block}.$update(${scope});
 	} else {
-		${block}.$destroy();
+		${block} && ${block}.$destroy();
 		${block} = condition_${cond}(${scope});
 		${block}.$create();
 		${block}.$mount(${root}, ${anchor});
