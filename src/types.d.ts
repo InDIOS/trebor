3357e0ec22f7eadd26364f1b381bc15508335f0d
Roberto Asiel Guevara Castañeda <robert04siel@gmail.com>
2018-06-23 10:55:18 -0400

    Removed unnecesary types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,29 +1,3 @@
-export interface Node {
-	type: 'element' | 'comment' | 'text';
-}
-
-export interface Element extends Node {
-	type: 'element';
-	tagName: string;
-	children: Node[];
-	attributes: Attribute[];
-}
-
-export interface Attribute {
-	key: string;
-	value?: string;
-}
-
-export interface Text extends Node {
-	type: 'text';
-	content: string;
-}
-
-export interface Comment extends Node {
-	type: 'comment';
-	content: string;
-}
-
 export interface CompilerOptions {
 	out?: string;
 	input?: string;
