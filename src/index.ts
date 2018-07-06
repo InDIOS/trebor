0c0183a89ac2ee1ceb49de04cbf7832e81b3d509
Roberto Asiel Guevara Castañeda <robert04siel@gmail.com>
2018-07-06 10:27:00 -0400

    Exported new dependencies

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,9 @@ import { kebabToCamelCases, capitalize } from './utilities/tools';
 import { TryStatement, FunctionDeclaration, FunctionExpression } from 'estree';
 
 const deps = `import { 
-	_$CompCtr, _$, _$d, _$a, _$add, _$as, _$r, _$ce, _$cse,	_$ct, _$cm, _$sa, _$ga, _$al, _$ul, _$rl, _$bc,
-	_$bs, _$f, _$e, _$is, _$ds, _$toStr, _$setRef, _$noop, _$isType, _$plugin
+	_$CompCtr, _$, _$d, _$a, _$add, _$as, _$r, _$ce, _$cse,	_$ct, _$cm,
+	 _$sa, _$ga, _$al, _$ul, _$rl, _$bc, _$bs, _$f, _$e, _$is, _$ds, _$toStr,
+	 _$setRef, _$noop, _$isType, _$plugin, _$isKey, _$bindGroup,_$emptyElse
 } from 'trebor/tools';`;
 const tools = readFileSync(join(__dirname, '../tools/index.js'), 'utf8');
 
