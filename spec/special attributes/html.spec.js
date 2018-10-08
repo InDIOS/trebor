c9adb675d3b0eb55a7cbd6cb4cfefd6b376c288d
Roberto Asiel Guevara Castañeda <robert04siel@gmail.com>
2018-10-08 12:19:10 -0400

    Revert "Ignored spec folder"
    This reverts commit 14d9213eb872ce2073f0632d70053aa3ea7f65e1.

---
+++ spec/special attributes/html.spec.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("../helpers/setup");
+const Component = require('../components/html.umd');
+describe('Component Html', () => {
+    let instance, divs;
+    beforeAll(done => {
+        instance = new Component();
+        instance.$mount('main');
+        done();
+    });
+    it('nodes without expression should be normally rendered', () => {
+        divs = document.querySelectorAll('div');
+        let div = divs.item(0);
+        expect(div.innerHTML).toBe('<p>Here\'s a very interesting note displayed in a lovely shadowed box.</p><p>Any kind of content here. Such as &lt;p&gt;, &lt;table&gt;. You name it!</p>');
+    });
+    it('nodes with expression should not escape expressions', () => {
+        divs = document.querySelectorAll('div');
+        let div_1 = divs.item(1), div_2 = divs.item(2);
+        expect(div_1.innerHTML).toBe('<p>Some paragraph with text undefined</p>');
+        expect(div_2.innerHTML).toBe('<p>Some paragraph with undefined and undefined expressions</p>');
+    });
+    it('`$html` should escape expressions', () => {
+        divs = document.querySelectorAll('div');
+        let div_1 = divs.item(3), div_2 = divs.item(4);
+        expect(div_1.innerHTML).toBe('<p $some-attr="someVar">Some paragraph with text intentional {{escaped}}</p>');
+        expect(div_2.innerHTML).toBe('<p $some-attr="someVar">Some paragraph with text from {{model}}</p>');
+    });
+});
