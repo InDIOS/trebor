b7178f962b0055fcba48c5ca22a82536d1d4f19a
Roberto Asiel Guevara Castañeda <robert04siel@gmail.com>
2019-04-29 12:36:15 +0200

    Removed examples folder and scripts

--- examples/counter/src/main.ts
+++
@@ -1,4 +0,0 @@
-import Counter from './components/counter.html';
-
-const counter = new Counter();
-counter.$mount('main');
--- examples/todomvc/src/main.ts
+++
@@ -1,7 +0,0 @@
-// import Calendar from './components/calendar.html';
-import Todo from './components/todo.html';
-
-const todo = new Todo();
-// const calendar = new Calendar();
-todo.$mount('main');
-// calendar.$mount('main');
