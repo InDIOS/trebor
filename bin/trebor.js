83c2dee820acfd0193663a4b0091e1b0bdab5abe
Roberto Asiel Guevara Castañeda <robert04siel@gmail.com>
2019-11-26 11:32:41 -0500

    Adopt new api and added some new options

--- bin/trebor.js
+++ bin/trebor.js
@@ -1,43 +1,88 @@
 #!/usr/bin/env node
-const yargs = require('yargs');
+
+const glob = require('glob');
 const { resolve } = require('path');
-const pkg = require('../package.json');
-const gnt = require('../build').default;
+const program = require('commander');
+const { parseFile } = require('../build');
 
-const options = yargs.options({
-	input: {
-		alias: 'i',
-		type: 'string',
-		demandOption: true,
-		describe: 'Path to the input file.'
-	},
-	output: {
-		alias: 'o',
-		type: 'string',
-		describe: 'Path to the output file.'
-	},
-	format: {
-		alias: 'f',
-		type: 'string',
-		choices: ['es', 'iif', 'umd', 'amd', 'cjs', 'system'],
-		describe: 'Format of the output.'
-	},
-	minify: {
-		alias: 'm',
-		type: 'boolean',
-		describe: 'Minify the output.'
-	}
-}).version(pkg.version).argv;
+program
+  // TO-DO: Put here parsers (css,html,js) and directives
+  .option('-i, --input <fileOrDirPath>', 'Path to input file or directory', dir => resolve(dir))
+  .option('-o, --output [fileOrDirPath]', 'Path to output file or directory (default: input directory)')
+  .option('-f, --format <OutputFormat>', 'Specify output format: esm, cjs o iif (default: iif)', val => {
+    return /^(esm|cjs|iif)$/i.test(val) ? val : '';
+  })
+  .option('-c, --comments', 'Include comments in parsed result file')
+  .option('-m, --minify', 'Optimize output with minification')
+  .option('-d, --directives [value]', 'A set of path to directive files', (dir, directives) => {
+    directives.push(require(resolve(dir)));
+    return directives;
+  }, [])
+  .version('1.0.0', '-v, --version')
+  .parse(process.argv);
 
-const cwd = process.cwd();
+const options = program.opts();
+
+if (!options.input) {
+  console.log('');
+  console.error('[Error]: An input file must be provided in argument -i o --input');
+  console.log('');
+  process.exit(1);
+}
+
+if (options.format === '') {
+  console.log('');
+  console.warn('[Warn]: An incorrect output format was provided');
+  console.warn('[Warn]: Format option will fallback to `iif` module');
+}
 
 if (options.input) {
-  gnt({
-    input: resolve(cwd, options.input),
-    out: options.output ? resolve(cwd, options.output) : undefined,
-    format: options.format,
-    minify: options.minify
-  });
-} else {
-  throw new Error('A input file must be provided.');
+  options.input = resolve(options.input);
 }
+
+if (options.output) {
+  options.output = resolve(options.output);
+}
+
+options.format = options.format || 'iif';
+options.minify = options.minify || false;
+options.comments = options.comments || false;
+
+const compilerOptions = {
+  minify: options.minify,
+  format: options.format,
+  comments: options.comments,
+  outDir: options.output,
+  optimize: true
+};
+
+const start = new Date();
+
+glob(options.input, { nonull: false }, (err, files) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  } else {
+    Promise.all(files.map(file => {
+      return new Promise((resolve, reject) => {
+        parseFile(file, { compilerOptions, directives: options.directives }, err => {
+          if (err) {
+            err.message = `${err.message} on file '${file}'`;
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+    })).then(wrotes => {
+      if (wrotes.length === files.length) {
+        const time = ((new Date()) - start) / 1000;
+        console.log(`[Info]: All work done successfully in ${time.toFixed(2)}s`);
+      }
+    }).catch(err => {
+      console.log(err);
+      console.error('\n[Error]: Something was wrong');
+      process.exit(1);
+    });
+  }
+});
